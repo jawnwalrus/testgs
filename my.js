25244cb5aa2d3d14602ab18b5ef353a72aef3a73
jawnwalrus <104670446+jawnwalrus@users.noreply.github.com>
2022-09-06 23:11:51 -0400

    Update my.js

--- my.js
+++ my.js
@@ -15,4 +15,27 @@ function testGS(){
 
 }
 
+function addGS(){
+
+  const url = "https://script.google.com/macros/s/AKfycbwlFnstqUezme15mtIzrM5Dn5lY1Pst8slGR902H-97HHMD-xE/exec"
+
+
+  fetch(url,{
+      method: 'POST',
+      mode: 'no-cors',
+      cache: 'no-cache',
+      //credentials: 'omit',
+      headers: {
+      'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      //referrerPolicy: 'no-referrer',
+      body: JSON.stringify({name: "John"})
+    });
+
+}
+
+document.getElementById("btn2").addEventListener("click",addGS);
+
+
 document.getElementById("btn").addEventListener("click",testGS);
